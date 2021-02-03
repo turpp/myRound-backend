9dc404f2ecc133fcfd943718e11dd87286561c7d
turpp <white.tripp@icloud.com>
2021-02-03 14:24:34 -0600

    delted summary route. also changed the show round to render round and the holes associated with it.

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,7 +1,7 @@
 class RoundsController < ApplicationController
     def show
         round = Round.find_by(id: params[:id])
-        render json: round, except: [:created_at, :updated_at]
+        render json: round, include: [:holes]
     end
 
     def create
